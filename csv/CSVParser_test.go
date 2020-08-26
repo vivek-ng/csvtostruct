c66829da54c033ca9eaac6dff6b9d70eaa0bb201
vivek-ng <vivek@shipt.com>
2020-08-26 17:21:32 -0500

    test for csv parser

--- csv/CSVParser_test.go
+++ csv/CSVParser_test.go
@@ -1,1 +1,24 @@
 package csv_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vivek-ng/csvtostruct/csv"
+)
+
+type testParser1 struct {
+	Field1 string `csv:"field1"`
+	Field2 int    `csv:"field2"`
+}
+
+func TestParser(t *testing.T) {
+	newParser, _ := csv.NewCSVStructer(&testParser1{}, []string{"field1", "field2"})
+	isValid := newParser.ValidateHeaders([]string{"field1", "field2"})
+	assert.Equal(t, isValid, true)
+	var parser testParser1
+	err := newParser.ScanStruct([]string{"apple", "43"}, &parser)
+	assert.Nil(t, err)
+	assert.Equal(t, parser.Field1, "apple")
+	assert.Equal(t, parser.Field2, 43)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vivek-ng/csvtostruct
 
 go 1.14
+
+require github.com/stretchr/testify v1.6.1
